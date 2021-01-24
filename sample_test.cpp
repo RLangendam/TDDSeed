6e4625b4c44b00ff59165b955f12a5eb347f51fa
Ralph Langendam <RLangendam@users.noreply.github.com>
2021-01-24 19:12:32 +0000

    Hello Boost

--- sample_test.cpp
+++ sample_test.cpp
@@ -1,6 +1,12 @@
 ﻿#include <catch2/catch.hpp>
+#include <boost/range/algorithm/copy.hpp>
+#include <boost/range/adaptor/transformed.hpp>
+#include <iterator>
 
 TEST_CASE("sample")
 {
-	REQUIRE(true == true);
+    std::vector<int> data{1, 2, 3};
+    std::vector<int> output;
+    boost::copy(data | boost::adaptors::transformed([](auto v){return 2*v;}), std::back_inserter(output));
+	REQUIRE(output == std::vector<int>{2, 4, 6});
 }
